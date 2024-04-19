de9e1d82bf47962795a37e4824a869687bfa88c7
Diversworld <eckhard@diversworld.eu>
2024-04-19 17:20:03 +0200

    Sprachdateien für check_invoice erstellt und aktualisiert

---
+++ contao/languages/de/tl_dw_check_invoice.php
@@ -0,0 +1,42 @@
+<?php
+
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DiCoMa.
+ *
+ * (c) Diversworld 2024 <eckhard@diversworld.eu>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/diversworld/contao-dicoma-bundle
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['tuv_legend'] = "TÜV-Termin Einstellungen";
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['vendor_legend'] = "Prüfunternehmen";
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['article_legend'] = "Preise für die TÜV-Prüfung";
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['addCheckInfo'] = ["Daten zum TÜV-Termin erfassen", "Informationen zum TÜV-Termin erfassen"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['addVendorInfo'] = ["Prüfungsunternehmen", "Informationen zum Prüfungsunternehmen erfassen."];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['addArticleInfo'] = ["Preisliste erstellen", "Preisliste für den TÜV-Termin erfassen"];
+
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['is_tuv_appointment'] = ["TÜV-Termin", "Dieser Event ein TÜV-Termin?"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['vendorName'] = ["Firma", "Firmenname des Prüfungsunternehmens"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['street'] = ["Straße", "Geben Sie die Straße ein."];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['postal'] = ["PLZ", "Geben Sie die PLZ ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['city'] = ["Ort", "Geben Sie den Ort ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['vendorEmail'] = ["E-Mail", "Geben Sie die E-Mail-Adresse ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['vendorPhone'] = ["Telefonnummer", "Geben Sie die Telefonnummer ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['vendorMobile'] = ["Mobilnnummer", "Geben Sie die Mobilnummer ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['checkArticles'] = ["TÜV-Preisliste", "Geben Sie die Preise für die Prüfungsartikel ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['articleName'] = ["Artikel", "Geben ie Artikelbezeichnung ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['articleNotes'] = ["Beschreibung", "Geben Sie eine Beschreibung ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['articlePriceNetto'] = ["Preis netto", "Geben Sie den Nettopreis ein"];
+$GLOBALS['TL_LANG']['tl_dw_check_invoice']['articlePriceBrutto'] = ["Preis brutto", "Der Bruttopreis wird beim speichern berechnet."];
