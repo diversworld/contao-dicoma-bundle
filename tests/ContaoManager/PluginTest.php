622fc0a85d3c43d3596f0c4ca2827765c300d718
Diversworld <eckhard@diversworld.eu>
2024-04-21 13:18:04 +0200

    PluginTest.php aktualisiert

--- tests/ContaoManager/PluginTest.php
+++ tests/ContaoManager/PluginTest.php
@@ -46,7 +46,6 @@ class PluginTest extends ContaoTestCase
         $this->assertCount(1, $bundles);
         $this->assertInstanceOf(BundleConfig::class, $bundles[0]);
         $this->assertSame(DiversworldContaoDicomaBundle::class, $bundles[0]->getName());
-        $this->assertSame([ContaoCoreBundle::class], $bundles[0]->getLoadAfter());
+        $this->assertSame([ContaoCoreBundle::class, ContaoCalendarBundle::class], $bundles[0]->getLoadAfter());
     }
-
 }
